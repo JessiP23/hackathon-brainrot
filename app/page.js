3d79ebf14adfd34ecbfe017fca58872f551cca59
JessiP23 <jessi316866@gmail.com>
2024-12-01 11:41:00 -0500

    data

--- app/page.js
+++ app/page.js
@@ -45,7 +45,7 @@ export default function Home() {
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
         <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-          <h1 className="text-2xl font-bold mb-5 text-center">Data Structures Mentor</h1>
+          <h1 className="text-2xl font-bold mb-5 text-center text-gray-800">Data Structures Mentor</h1>
 
           <div className="mb-5 text-gray-800">
             <label htmlFor="data-structure" className="block mb-2">Select Data Structure:</label>
